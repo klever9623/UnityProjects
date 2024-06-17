924645b028c67439b84c6942573996a8fea16d7b
Zhenya848 <BelovEA100207@yandex.ru>
2024-06-17 14:12:18 +0300

    Commit4

---
+++ DriftGameUnityScripts/CameraScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform Car;
+    [SerializeField] private Vector3 _offset = new Vector3();
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRotate;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = Car.TransformPoint(_offset);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _speed * Time.fixedDeltaTime);
+
+        Vector3 direction = Car.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, _speedRotate * Time.fixedDeltaTime);
+    }
+}
